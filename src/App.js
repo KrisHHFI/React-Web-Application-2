09143cda112dedfe2cbf4556eb56932c4715c19a
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2024-01-03 17:26:42 +0200

    Basic page structure defined

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+// Stylesheets
 import './App.css';
-
+import './StyleSheets/Header.css';
+import './StyleSheets/Content.css';
 import './StyleSheets/Clock.css';
+// Components
+import Header from './Components/Header';
+import Content from './Components/Content';
 import Clock from './Components/Clock';
 
 function App() {
   return (
-    <div className="App">
-       <Clock />
+    <div className="Wrapper">
+      <Header />
+      <Content />
+      {/* <Clock /> */}
     </div>
   );
 }
