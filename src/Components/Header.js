f08a66036d4cf4d5d681c229fb29064b4a0c3631
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2024-01-04 21:15:40 +0200

    Header, active link effect

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CompanyLogo from '../Images/Company-Logo.png';
 import CompanyText from '../Images/Company-Text.png';
 
 export default function Header() {
+    const [activeLink, setActiveLink] = useState('Products');
+
+    const handleLinkClick = (linkName) => {
+        setActiveLink(linkName);
+        return activeLink === linkName;
+    };
 
     return (
         <div className="Header-Container">
             <div className="Header-Item">
-                <img src={CompanyLogo} className="CompanyLogo" />
+                <img src={CompanyLogo} alt="Company Logo" className="CompanyLogo" />
             </div>
             <div className="Header-Item">
-                <img src={CompanyText} className="CompanyText" />
+                <img src={CompanyText} alt="Company Text" className="CompanyText" />
             </div>
             <div className="Header-Item">
                 <div className="Nav-Bar">
-                    <div className="Nav-Item">
-                        <a href="">Products</a>
+                    <div className={`Nav-Item ${activeLink === 'Products' ? 'active' : ''}`}>
+                        <a href="#" onClick={() => handleLinkClick('Products')}>Products</a>
                     </div>
-                    <div className="Nav-Item">
-                        <a href="">About</a>
+                    <div className={`Nav-Item ${activeLink === 'About' ? 'active' : ''}`}>
+                        <a href="#" onClick={() => handleLinkClick('About')}>About</a>
                     </div>
-                    <div className="Nav-Item">
-                        <a href="">Contact</a>
+                    <div className={`Nav-Item ${activeLink === 'Contact' ? 'active' : ''}`}>
+                        <a href="#" onClick={() => handleLinkClick('Contact')}>Contact</a>
                     </div>
                 </div>
             </div>
