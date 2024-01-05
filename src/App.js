18e318ab5ed73db42ad7866a67dc3c93177e8580
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2024-01-05 09:45:01 +0200

    The active link now determines which page/file is returned inside the content div

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 // Stylesheets
 import './StyleSheets/App.css';
 import './StyleSheets/Header.css';
@@ -5,14 +6,36 @@ import './StyleSheets/Content.css';
 import './StyleSheets/Clock.css';
 // Components
 import Header from './Components/Header';
+import Products from './Components/Products';
+import About from './Components/About';
+import Contact from './Components/Contact';
 import Clock from './Components/Clock';
 
 function App() {
+  const [activeLink, setActiveLink] = useState('Products');
+
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
+
+  const renderContent = () => {
+    switch (activeLink) {
+      case 'Products':
+        return <Products />;
+      case 'About':
+        return <About />;
+      case 'Contact':
+        return <Contact />;
+      default:
+        return <Products />;
+    }
+  };
+
   return (
     <div className="Wrapper">
-      <Header />
+      <Header activeLink={activeLink} onLinkClick={handleLinkClick} />
       <div className="Content-Container">
-        Content
+        {renderContent()}
         {/* <Clock /> */}
       </div>
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CompanyLogo from '../Images/Company-Logo.png';
 import CompanyText from '../Images/Company-Text.png';
 
-export default function Header() {
-    const [activeLink, setActiveLink] = useState('Products');
-
-    const handleLinkClick = (linkName) => {
-        setActiveLink(linkName);
-        return activeLink === linkName;
-    };
-
+export default function Header({ activeLink, onLinkClick }) {
+    
     return (
         <div className="Header-Container">
             <div className="Header-Item">
@@ -21,13 +15,13 @@ export default function Header() {
             <div className="Header-Item">
                 <div className="Nav-Bar">
                     <div className={`Nav-Item ${activeLink === 'Products' ? 'active' : ''}`}>
-                        <a href="#" onClick={() => handleLinkClick('Products')}>Products</a>
+                        <a href="#" onClick={() => onLinkClick('Products')}>Products</a>
                     </div>
                     <div className={`Nav-Item ${activeLink === 'About' ? 'active' : ''}`}>
-                        <a href="#" onClick={() => handleLinkClick('About')}>About</a>
+                        <a href="#" onClick={() => onLinkClick('About')}>About</a>
                     </div>
                     <div className={`Nav-Item ${activeLink === 'Contact' ? 'active' : ''}`}>
-                        <a href="#" onClick={() => handleLinkClick('Contact')}>Contact</a>
+                        <a href="#" onClick={() => onLinkClick('Contact')}>Contact</a>
                     </div>
                 </div>
             </div>
