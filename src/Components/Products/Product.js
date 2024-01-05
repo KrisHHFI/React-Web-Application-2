fdcc5aec47b99dc6599be9278e09275674783ba1
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2024-01-05 23:36:14 +0200

    Products place holders improved

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -5,10 +5,18 @@ export default function Product() {
     return (
         <div class="Product">
             <div class="Product-Section">
-                Title
+                Commodo Consequat
             </div>
             <div class="Product-Section">
-                Text and button
+                <div class="Product-Section-Divider">
+                    <div class="Product-Text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </div>
+                </div>
+                <div class="Product-Section-Divider">
+                    <a class="Product-Link" href="#">Lorem</a>
+                </div>
             </div>
         </div>
     );
