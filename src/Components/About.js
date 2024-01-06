1ff5293f8a052a836c9a0f638246499cd4deaa0a
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2024-01-06 22:36:09 +0200

    Titles and dividers added to About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,6 +2,9 @@ import React from 'react';
 import OfficeInterior from '../Images/About/OfficeInterior.png';
 import ConstructionSite from '../Images/About/ConstructionSite.png';
 import HelsinkiSkyline from '../Images/About/HelsinkiSkyline.jpg';
+import Who from '../Images/About/Who.png';
+import Mission from '../Images/About/Mission.png';
+import Office from '../Images/About/Office.png';
 
 export default function About() {
 
@@ -9,12 +12,18 @@ export default function About() {
         <div class="About-Container">
             <div class="About-Item">
                 <img src={OfficeInterior} alt="Office Interior" className="CoverImage" />
+                <img src={Who} alt="Office Interior" className="AboutImageTitle" />
+                <div class="About-Divider"></div>
             </div>
             <div class="About-Item">
                 <img src={ConstructionSite} alt="ConstructionSite" className="CoverImage" />
+                <img src={Mission} alt="Office Interior" className="AboutImageTitle" />
+                <div class="About-Divider"></div>
             </div>
             <div class="About-Item">
                 <img src={HelsinkiSkyline} alt="Credit to Tapio Haaja, Pexels." className="CoverImage" />
+                <img src={Office} alt="Office Interior" className="AboutImageTitle" />
+                <div class="About-Divider"></div>
             </div>
             <div class="About-Item">
             </div>
